edbcfb8b82b8ba96452c040ed101f02fb3a73472
Carlo Palladino”git config --global user.name CarlosPalladino” <carlosezequiel19palladino@gmail.com>
2022-12-04 15:16:08 -0300

    aplicnado cambios en el home

--- vie-burbuja/src/App.jsx
+++ vie-burbuja/src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
+ import Home from './Home'
 import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-hello word
-    </div>
-  )
+  return ( <>
+    <Home name="carlos">
+<p> se esfuerza mucho</p>
+    </Home>
+  </> )
 }
 
 export default App
---
+++ vie-burbuja/src/Home.jsx
@@ -0,0 +1,10 @@
+function Home (props){
+return (<> 
+<h1> 
+</h1>
+ {props.children} 
+</>
+)
+
+}
+export default Home
